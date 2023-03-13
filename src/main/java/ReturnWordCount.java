bdd71c588336fa5819add8861bc723f14aad0994
gcruzBwise <gcruz28@wgu.edu>
2023-03-13 00:50:21 +0000

    Complete

--- src/main/java/ReturnWordCount.java
+++ src/main/java/ReturnWordCount.java
@@ -1,10 +1,15 @@
-
+import java.util.*;
 public class ReturnWordCount {
     /**
      * @param in A String representing a sentence, with words delineated by spaces.
      * @return return the amount of words in a string.
      */
     public int count(String in){
-        return 0;
+        if (in == null || in.isEmpty()){
+           return 0; 
+        }
+
+        String [] words = in.split("\\s+");
+        return words.length;
     }
 }
